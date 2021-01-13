261861397fed28230aca41f256b1778b79164bf2
milimehta <milimehta95@gmail.com>
2021-01-13 22:50:27 +0530

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mili Mehta on 23/02/19.
+//  Copyright © 2019 Mili Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
